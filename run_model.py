212ebf3908aca4ec2e4650d472e8fc671b514c88
nsamelson <salocin.sam@gmail.com>
2025-04-07 15:48:51 +0200

    run model

--- run_model.py
+++ run_model.py
@@ -1,25 +1,79 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import jax.numpy as jnp
+import h5py
+from sklearn.metrics import mean_squared_error
+
+import run_simulation
 
 
 
-
-#TODO: change with RMSE or MSE
-def compute_bic(y_true, y_pred, num_params):
-    n = len(y_true)
-    residuals = y_true - y_pred
-    rss = np.sum(residuals**2)
-    bic = num_params * np.log(n) + n * np.log(rss / n)
+def compute_bic(n, mse, num_params):
+    bic = n * np.log(mse) + num_params * np.log(n)
     return bic
 
 
+# MATLAB loader function
+def load_matlab_data(filename):
+    with h5py.File(filename, 'r') as f:
+        data = {key: f[key][()] for key in f.keys()}
+    return data
 
 
-# Suppose X, Y are your data
-# model = LinearRegression()
-# model.fit(X, Y)
-# y_pred = model.predict(X)
+def main():
 
-# k = X.shape[1] + 1  # +1 for the intercept
-# bic_value = compute_bic(Y, y_pred, k)
-# print("BIC:", bic_value)
+    # Load real data
+    data = load_matlab_data("data/data_prbs_30000hz_1000000_20190207_013502.mat")
+    I = jnp.array(data.get("I"))[0]
+    y_true = data.get("U4")
+    durations = data.get("duration")[0]
+    fss = data.get("fs")[0]
+
+
+    # bandwidth freq
+    fbs = np.array([30000])
+
+
+    # Simulation frequency parameters
+    # fbs = np.array([1]) # bandwidths of the DRBS signal
+    # durations = [10]    # duration of the sampling (s)
+    # fss = [2000]         # sampling frequency
+
+    # Electrical circuit blocks
+    Rs = jnp.array(3.)                      # Resistance of supply?
+    R = jnp.array([1., 2., 3.])             # Resistance
+    C = jnp.array([.1, 1., 10.])            # Capacitance
+    alpha = jnp.array([0.88, 0.82, 0.99])    # Fractional factor linked with the capacitance
+
+    print(Rs)
+
+    params = {
+        "fbs": fbs,
+        "durations": durations,
+        "fss": fss,
+        "Rs": Rs,
+        "R": R,
+        "C": C,
+        "alpha": alpha
+    }
+
+    # run simulation
+    y_pred = run_simulation.main(I,params)
+
+    # compute metrics
+    n = len(y_true)   
+    mse = mean_squared_error(y_true, y_pred)
+    num_params = 1 + len(R) + len(C) + len(alpha)
+    bic = compute_bic(n,mse,num_params)
+
+    print(bic, mse)
+
+
+    # Save file
+    np.savez("output/signals.npz",x=I,y_true=y_true, y_pred=y_pred, params= params, allow_pickle=True)
+
+
+
+
+if __name__ == "__main__":
+    main()
