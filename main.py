4583f15eb5d57b2643b49ac1440108a8b1e0c58f
nsamelson <salocin.sam@gmail.com>
2025-05-15 12:14:11 +0200

    run on all cells

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import glob
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 import argparse
@@ -59,14 +60,15 @@ def data_stream(signals, batch_size):
     for i in range(0, len(signals[0]), batch_size):
         yield (signals[j][i:i+batch_size] for j in range(len(signals)))
 
-def step(params, opt_state, I, U_train, U_val, optimizer, fs, minibatch=True):
+def step(params, opt_state, I, U_train, U_val, optimizer, fs, minibatch=True, train_cell="U1"):
 
     if minibatch:
         batches = data_stream([I, U_train], 2000)
     else:
         batches = [(I, U_train)] 
 
-    tot_loss = 0
+    tot_loss = 0.
+    avg_val_loss = 0.
     for I_batch, U_batch in batches:
         # still differentiate w.r.t. params
         loss, grads = jax.value_and_grad(compute_loss)(params, I_batch, U_batch, fs)
@@ -81,41 +83,46 @@ def step(params, opt_state, I, U_train, U_val, optimizer, fs, minibatch=True):
         params['alpha'] = jnp.clip(params['alpha'], a_min=0.6,  a_max=1.0)
         params['C']     = jnp.clip(params['C'],     a_min=1.0,  a_max=4.0)
 
+    
     # Simulate once for full val loss
-    y_pred_val = sim_z(I=I, fs=fs, **params)
-    val_loss = jnp.sum(optax.squared_error(y_pred_val, U_val))
+    if train_cell == "U1":
+        y_pred_val = sim_z(I=I, fs=fs, **params)
 
-    return params, opt_state, tot_loss, val_loss
+        val_losses = [jnp.sum(optax.squared_error(y_pred_val, U_cell_val)) for U_cell_val in U_val]
+        avg_val_loss = jnp.mean(jnp.array(val_losses))
+
+    return params, opt_state, tot_loss, avg_val_loss
 
 
 
-def train_loop(params, I, U_train, U_val, fs, num_steps=1000):
+def train_loop(params, I, U_train, U_val, fs, num_steps=1000, train_cell="U1"):
     optimizer = make_optimizer(params)
     opt_state = optimizer.init(params)
 
     losses = []
-    val_losses = []
-    early_stopper = EarlyStopping(patience=30, min_delta=1e-4)
+    avg_val_losses = []
+    early_stopper = EarlyStopping(patience=30, min_delta=1e-5)
     pbar = tqdm(range(num_steps), desc="Training")
 
     for _ in pbar:
-        params, opt_state, loss, val_loss = step(params, opt_state, I, U_train, U_val, optimizer, fs)        
+        params, opt_state, loss, avg_val_loss = step(params, opt_state, I, U_train, U_val, optimizer, fs, train_cell)        
         losses.append(loss.item())
-        val_losses.append(val_loss.item())
+        avg_val_losses.append(avg_val_loss.item())
+
+        # Early stop
+        early_stopper(loss.item())
+        if early_stopper.should_stop:
+            print(f"Early stopping triggered after {early_stopper.patience} epochs without improvement.")
+            break
 
         pbar.set_description(
-            f"Train loss={loss:.4e}, val loss={val_loss:.4e}, Rs={params['Rs']:.4f},"
+            f"Train loss={loss:.4e}, Rs={params['Rs']:.4f},"
             f"R={[f'{r:.4f}' for r in params['R'].tolist()]}, "
             f"C={[f'{c:.4f}' for c in params['C'].tolist()]}, "
             f"a={[f'{a:.4f}' for a in params['alpha'].tolist()]}"
         )
-        # Early stop
-        early_stopper(val_loss.item())
-        if early_stopper.should_stop:
-            print(f"Early stopping triggered after {early_stopper.patience} epochs without improvement.")
-            break
             
-    return params, losses, val_losses
+    return params, losses, avg_val_losses
 
 
 def load_data(path, freq):
@@ -146,8 +153,9 @@ def correct_signal(orig_signal):
 def main(model_name, N, iters, freq, debug, sampling_frequency):
     work_dir = os.getcwd()
     key = 42
-    el = 4000
+    el = 2000
     rng = jax.random.PRNGKey(key)
+    cells = ["U1","U2","U3","U4","U5","U6"]
 
     # get data
     data = load_data(os.path.join(work_dir, "data"), freq)
@@ -155,32 +163,55 @@ def main(model_name, N, iters, freq, debug, sampling_frequency):
 
     # decimate and correct offset
     I = correct_signal(decimate_signal(data["I"],fs,sampling_frequency))
-    U_train = decimate_signal(data["U1"],fs,sampling_frequency)
-    U_val = decimate_signal(data["U2"],fs,sampling_frequency)
+    U_cells = [decimate_signal(data[cell],fs,sampling_frequency) for cell in cells]
 
     if debug:
-        U_train= U_train[:el]
-        U_val= U_val[:el]
+        U_cells = [cell[:el] for cell in U_cells]
         I= I[:el]
 
+    # normalise
     I -= jnp.mean(I)
-    U_train -= jnp.mean(U_train)
-    U_val -= jnp.mean(U_val)
+    U_cells = [cell - jnp.mean(cell) for cell in U_cells]
 
     # init parameters
     params = {
         'Rs':    jnp.array(3e-3),                   # initial supply resistance
         'R':     jnp.ones((N,)) * 5e-2 ,             # block resistances
-        # 'C':     jnp.log(jnp.ones((N,)) * 100), 
         'C':     jnp.log(jnp.array([10.,100.,1000,500.,500.,500.])[:N]) ,            # block capacitances
         'alpha': jnp.ones((N,)) * 0.75,             # fractional factors
     }
 
-    # run training
-    trained_params, losses, val_losses = train_loop(params, I, U_train, U_val, num_steps=iters, fs=fs)
+    history = {}
 
-    # final simulation
-    y_pred = sim_z(I=I,fs=fs, **trained_params)
+    # run training for each cell
+    for i in range(len(U_cells)):
+        U_train = U_cells[i]
+        U_val = U_cells[:i] + U_cells[i+1:]
+        cell = cells[i]
+
+        trained_params, losses, avg_val_losses = train_loop(params, I, U_train, U_val, num_steps=iters, fs=fs, train_cell=cell)
+
+        # compute metrics
+        y_pred = sim_z(I=I,fs=fs, **trained_params)
+        rmse  = run_model.root_mean_squared_error(U_train, y_pred)
+        cae   = run_model.cumulative_absolute_error(U_train, y_pred)
+        bic = run_model.compute_bic(len(U_train),rmse,3*N+1)
+
+        history[cell] = {
+            "losses": losses,
+            "best_params": trained_params,
+            "rmse": rmse,
+            "cae": cae,
+            "bic": bic
+        }
+
+        if cell == "U1":
+            history["avg_val_losses"] = avg_val_losses
+            history["avg_val_rmse"] = np.mean(np.array([run_model.root_mean_squared_error(U_cell, y_pred) for U_cell in U_val]))
+            history["avg_val_cae"] = np.mean(np.array([run_model.cumulative_absolute_error(U_cell, y_pred) for U_cell in U_val]))
+            history["avg_val_bic"] = run_model.compute_bic(len(U_train),history["avg_val_rmse"],3*N+1)
+    
+    print(history.keys())    
 
     config = {
         "model_name": model_name,
@@ -193,8 +224,7 @@ def main(model_name, N, iters, freq, debug, sampling_frequency):
     }
 
     # compute metrics
-    rmse  = run_model.root_mean_squared_error(U_val, y_pred)
-    cae   = run_model.cumulative_absolute_error(U_val, y_pred)
+    
     print(f"\nTrained params: {trained_params}")
     print(f"Final RMSE: {rmse:.4e},   CAE: {cae:.4f}")
 
@@ -202,7 +232,7 @@ def main(model_name, N, iters, freq, debug, sampling_frequency):
     # --- plot loss curve ---
     plt.figure(figsize=(6,4))
     plt.plot(losses, label="training loss")
-    plt.plot(val_losses, label="validation loss")
+    plt.plot(avg_val_losses, label="validation loss")
     # plt.yscale('log')
     plt.xlabel("Step")
     plt.ylabel("Squared–Error Loss")
